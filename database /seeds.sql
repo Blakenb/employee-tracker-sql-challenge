4ed9b6f57177a79361cfc9a098567969881a8d6c
Blakenb <blakenb87@gmail.com>
2025-03-04 18:50:05 -0600

    trying to get this to work

--- database /seeds.sql
+++ database /seeds.sql
@@ -1,23 +1,23 @@
+-- Insert departments
 INSERT INTO department (name)
 VALUES ('Graphic Design'),
        ('Video Game Development'),
-       ('Voice Acting'),
-       ('Script Writing'),
-       ('Management');
+       ('Full stack Dev'),
+       ('Customer service'),
+       ('Project Management');
 
-
+-- Insert roles
 INSERT INTO role (title, salary, department_id)
 VALUES ('Graphic Designer', 80000, 1),
        ('Video Game Developer', 80000, 2),
-       ('Voice Actor', 60000, 3),
-       ('Script Writer', 70000, 4),
-       ('Manager', 40000, 5);
-  
+       ('Full stack Dev', 110000, 3),
+       ('Customer service', 70000, 4),
+       ('Project Manager', 100000, 5);
 
+-- Insert employees
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('', '', 1, 5),
-       ('', '', 2, 5),
-       ('', '', 3, 5),
-       ('', '', 4, 5),
-       ('', '', 5, null);
-      
+VALUES ('Isabel', 'Loving', 1, null),
+       ('Bobby', 'Little', 2, 1),
+       ('James', 'Hill', 3, 1),
+       ('Billy', 'Bob', 4, 1),
+       ('James', 'Brown', 5, null);
